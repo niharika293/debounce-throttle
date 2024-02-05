ded27fba029a715cf66315171979deface8a1f80
Niharika Gurnani <niharika.gurnani@solvezy.com>
2024-02-05 18:24:28 +0530

    Basic use case - debounce & throttle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {debounceTime, distinctUntilChanged, throttleTime} from 'rxjs/operators'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'debounce-throttle';
+  searchText  = new FormControl('');
+
+  constructor(){
+
+  }
+
+  ngOnInit() {
+    // debouncing 
+    // this.searchText.valueChanges.pipe(debounceTime(400),distinctUntilChanged()).subscribe((res) => {
+    //   console.log("res", res);
+    //   // any BE api call can be made here
+    // }, (err) =>{
+    //   console.log(err);
+    // });
+    // Throttling
+    this.searchText.valueChanges.pipe(throttleTime(400),distinctUntilChanged()).subscribe((res) => {
+      console.log("res", res);
+    } , (err) => {
+      console.log(err);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+
 
 @NgModule({
   declarations: [
@@ -10,8 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
+  exports : [ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
